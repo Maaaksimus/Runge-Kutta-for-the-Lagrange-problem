9224b5096ac0f66c2e2901b68abd800c5b6e2ef9
Maksim Isaev <plowecz@mail.ru>
2022-12-02 22:02:40 +0300

    newton

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 	vector<double> u;
 	int n = 20, max_rate = 0;
 	char *endptr;
-	double h, err_rate = 0, solve_rate = 0, e[2];
+	double h, err_rate = 0, solve_rate = 0, C[2];
 
 	printf("Enter the number of points: ");
 	scanf("%d", &n);
@@ -32,6 +32,58 @@ int main() {
 	return 0;
 }
 
-void RungeKutta() {
+// double diff(double f(double), double x) {
+//     return (f(x + EPS) - f(x)) / EPS;
+// }
 
+void RungeKutta(int n, vector<vector<double> > *x, double C1, double C2, double alph) {
+
+}
+void Newton(double *C, double alph) { // method "ready"
+    
+    vector<vector<double> > x_from_l(4);
+    vector<vector<double> > x_from_r(4);
+    vector<vector<double> > x(4);
+    double Jac_F[2][2], prev[2];
+    int n;
+
+    C[0] = C_1_default;
+    C[1] = C_2_default;
+
+    do {
+        prev[0] = C[0];
+        prev[1] = C[1];
+
+        RungeKutta(n, &x_from_r, C[0] - EPS, C[1], alph);
+        RungeKutta(n, &x_from_l, C[0] + EPS, C[1], alph);
+        Jac_F[0][0] = (x_from_l[0][n - 1] - x_from_r[0][n - 1]) / EPS;
+        Jac_F[0][1] = (x_from_l[2][n - 1] - x_from_r[2][n - 1]) / EPS;
+
+        for (int i = 0; i < 4; i ++) {
+            x_from_l[i].clear();
+            x_from_r[i].clear();
+        }
+
+        RungeKutta(n, &x_from_r, C[0], C[1] - EPS, alph);
+        RungeKutta(n, &x_from_l, C[0], C[1] + EPS, alph);
+        Jac_F[1][0] = (x_from_l[0][n - 1] - x_from_r[0][n - 1]) / EPS;
+        Jac_F[1][1] = (x_from_l[2][n - 1] - x_from_r[2][n - 1]) / EPS;
+
+        invJac(Jac_F);
+        RungeKutta(n, &x, C[0], C[1], alph);
+
+        C[0] -= Jac_F[0][0]*x[0][n - 1] + Jac_F[0][1]*x[2][n - 1];
+        C[1] -= Jac_F[1][0]*x[0][n - 1] + Jac_F[1][1]*x[2][n - 1];
+    } while(sqrt((C[0] - prev[0])*(C[0] - prev[0]) + (C[1] - prev[1])*(C[1] - prev[1])) > 1e-10);
+}
+
+void invJac(double J[2][2]) {
+    double buf;
+
+    buf = J[0][0];
+
+    J[0][0] = J[1][1];
+    J[0][1] *= -1;
+    J[1][0] *= -1;
+    J[1][1] = buf;
 }
--- mylib.h
+++ mylib.h
@@ -1,4 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#define EPS 1e-7
+#define C_1_default 1
+#define C_2_default 1
+
 using namespace std;
