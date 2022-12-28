8d7d02dbdd54d950c6b017691bf492612e6e83a4
Maaaksimus <85172833+Maaaksimus@users.noreply.github.com>
2022-12-28 23:45:56 +0300

    vectors

--- mylib.h
+++ mylib.h
@@ -9,13 +9,11 @@
 #include <iomanip>
 #include <ios>
 
-#define EPS 1e-8
+#define EPS 1e-15
 #define DIFF 1e-10
-#define gig 3.14159265358979323846 // как наладить точность?
-// #define C_1_default -(pow(gig, 4) / 384. - 2) / gig// -----//-----
-#define C_1_default 0.5558742601793006
-#define C_2_default -0.3926990816987242
-// #define C_2_default -1 * gig / 8. // начальные значения, которые надо рассчитать
+// #define M_PI 3.14159265358979323846 как наладить точность?
+#define C_1_default -(pow(M_PI, 4) / 384. - 2) / M_PI// -----//-----
+#define C_2_default -1 * M_PI_4 / 2. // начальные значения, которые надо рассчитать
 #define ALPHA 0
 #define K 32
 
@@ -23,8 +21,13 @@ using namespace std;
 
 MyVector f(MyVector k, double alph);
 void countK(MyVector *k, double h, MyVector y, double alph);
-MyVector vec2Vec(vector<double> v);        
+MyVector vec2Vec(vector<double> v);
 
-void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, vector<double> &h);
+MyVector countStep(double h, MyVector y, double alph);
+double L(MyVector x_curr, MyVector x_next, double alph);
+
+void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, vector<double> &H);
+void RungeKutta2(vector<MyVector> &X, double C1, double C2, double alph, vector<double> &H);
+
 void shooting(double *C, double alph);
 void invJac(double J[2][2]);
