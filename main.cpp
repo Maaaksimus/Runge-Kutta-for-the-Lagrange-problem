a01dd097d3c99759bcdefde469673f514d97e3d7
Maaaksimus <85172833+Maaaksimus@users.noreply.github.com>
2022-12-28 23:45:26 +0300

    vectors
    works rather good for zero

--- main.cpp
+++ main.cpp
@@ -4,38 +4,36 @@ int main() {
     ofstream x1("x1.txt"), x3("x3.txt"), t_x1("targ1.txt"), t_x3("targ3.txt");
     vector<double> h;
 	vector<vector<double> > x;
-	double C[2], alph = 0, t, s = 0, pos = 0, x_1, x_3, s2 = 0;
+    vector<MyVector> X;
+	double C[2], alph = 0, t, s = 0, pos = 0;
 
 	// shooting(C, alph);
     // cout << C[0] << " " << C[1] << endl;
 	cout << "t1" << endl;
     // RungeKutta(x, C[0], C[1], alph, h);
     cout << "C at the start: " << C_1_default << " " << C_2_default << endl;
-    RungeKutta(x, C_1_default, C_2_default, alph, h);
+    RungeKutta2(X, C_1_default, C_2_default, alph, h);
 
     // for (int i = 0; i < 4; i ++) {
     //     cout << x[x.size() - 1][i] << " ";
     // }
 
-    for (int i = 0; i < x.size() - 1; i ++) {
-        t = pos;
-        s += (0.5 * (x[i][0] + x[i + 1][0]) + pow(0.5 * (x[i][2] + x[i + 1][2]), 2)) * h[i];
-        
-        // x_1 = (-1/2.)*(pow(t,4) / 24. - gig / 4. * pow(t,3) / 6. + C_1_default*(-2)*t);
-        // x_3 = (-1/2.)*(pow(t,2) / 2. - gig / 4. * t);
-        
-        x1 << pos << " " << x[i][0] << endl;
-        x3 << pos << " " << x[i][2] << endl;
-        t_x1 << pos << " " << (-1/2.)*(pow(t,4) / 24. - gig / 4. * pow(t,3) / 6. + C_1_default*(-2)*t) << endl;
-        t_x3 << pos << " " << (-1/2.)*(pow(t,2) / 2. - gig / 4. * t) << endl;
-        pos += h[i];
+    for (int i = 0; i < X.size() - 1; i ++) {
+        // s += (0.5 * (x[i][0] + x[i + 1][0]) + pow(0.5 * (x[i][2] + x[i + 1][2]), 2)) * h[i];
+        // x1 << pos << " " << x[i][0] << endl;
+        // x3 << pos << " " << x[i][2] << endl;
+        // t = pos;
+        // t_x1 << pos << " " << (-1/2.)*(pow(t,4) / 24. - M_PI_4 * pow(t,3) / 6. + C_1_default*(-2)*t) << endl;
+        // t_x3 << pos << " " << (-1/2.)*(pow(t,2) / 2. - M_PI_4 * t) << endl;
+        // pos += h[i];
+        s += L(X[i], X[i+1], alph) * h[i];
     }
 
-    cout << "in 0: " << x[0][1] << " " << x[0][3] << " " << endl;
-    cout << "in pi/2: " << x[x.size() - 1][1] << " " << x[x.size() - 1][3] << " " << endl;
+    cout << "in 0: "; X[0].print();
+    cout << "in pi/2: "; X[X.size() - 1].print();
     cout << "Int: " << s << endl;
-    // t = gig / 2.;
-    // cout << (-1/2.)*(pow(t,3) / 6. - gig / 4. * pow(t,2) / 2. + C_1_default*(-2)) << endl;
+    // t = M_PI / 2.;
+    // cout << (-1/2.)*(pow(t,3) / 6. - M_PI / 4. * pow(t,2) / 2. + C_1_default*(-2)) << endl;
 
 	return 0;
 }
@@ -46,7 +44,6 @@ MyVector f(MyVector k, double alph) {
 	KK.x[1] = k.x[2];
 	KK.x[2] = (-1)*k.x[3];
 	KK.x[3] = exp(-alph * pow(k.x[0], 2)) * (1 - 2 * alph * pow(k.x[0], 2)) / 2.;
-    // KK.x[3] = 1 / 2.;
     // KK.print();
     return KK;
 }
@@ -76,13 +73,13 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
 
     x.push_back(v);
 	
-	x[0].push_back(0)   ;
+	x[0].push_back(0);
     x[0].push_back(C1);
     x[0].push_back(0);
     x[0].push_back(C2);
     // cout << C1 << " " << C2 << endl;
     
-    // while(gig / 2. - pos > EPS) {
+    // while(M_PI / 2. - pos > EPS) {
 
     do {
         step ++;
@@ -91,14 +88,13 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
 
         // cout << x.size() - 1 << endl;
         buf = vec2Vec(x[x.size() - 1]);
-        buf.print();
 
         // cout << "t3" << endl;
 
         do {
 
-            if (pos + h > gig / 2.) {
-                h = gig / 2. - pos;
+            if (pos + h > M_PI_2) {
+                h = M_PI_2 - pos;
                 fin = 1;
             }
 
@@ -119,14 +115,6 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
             k_full[3] = h*f(buf + (-1)*k_full[1] + 2*k_full[2], alph);
             k_full[4] = h*f(buf + (1 / 27.) * (7*k_full[0] + 10*k_full[1] + k_full[3]), alph);
             k_full[5] = h*f(buf + (1 / 625.) * (28*k_full[0] + (-125)*k_full[1] + 546*k_full[2] + 54*k_full[3] + (-378)*k_full[4]), alph);
-
-            k_full[0] = h*f(buf, alph);
-            k_full[1] = h*f(buf + (1 / 2.) * k_full[0], alph);
-            k_full[2] = h*f(buf + (1 / 4.) * (k_full[0] + k_full[1]), alph);
-            k_full[3] = h*f(buf + (-1)*k_full[1] + 2*k_full[2], alph);
-            k_full[4] = h*f(buf + (1 / 27.) * (7*k_full[0] + 10*k_full[1] + k_full[3]), alph);
-            k_full[5] = h*f(buf + (1 / 625.) * (28*k_full[0] + (-125)*k_full[1] + 546*k_full[2] + 54*k_full[3] + (-378)*k_full[4]), alph);
-
 /*
             k_half_1[0] = (h / 2.)*f(buf, alph);
             k_half_1[1] = (h / 2.)*f(buf + (1 / 2.) * k_half_1[0], alph);
@@ -173,7 +161,7 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
             }
             */
 
-            out << "h: " << h << " " << E.x[0] << " "<< E.x[1] << " "<< E.x[2] << " "<< E.x[3] << " pos " << pos << " step: " << step << endl;
+            out << "h: " << h << " " << E.x[0] << " "<< E.x[1] << " "<< E.x[2] << " "<< E.x[3] << " pos " << pos << endl;
             sw = 1;
 
         } while (sw == 0);
@@ -184,14 +172,11 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
 
         H.push_back(h);
 
-        buf2 = buf + (1./24.)*k_full[0] + (5./48.)*k_full[3] + (27./56.)*k_full[4] + (125./336.)*k_full[5];
-
         for (int i = 0; i < 4; i ++) {
             // x[step].push_back(x[step - 1][i] + 1. / 336. * (14*k_full[0].x[i] + 35*k_full[3].x[i] + 162*k_full[4].x[i] + 125*k_full[5].x[i]));
             // x[step].push_back(x[step - 1][i] + 1. / 336. * (14*k_full[0].x[i] + 35*k_full[3].x[i] + 162*k_full[4].x[i] + 125*k_full[5].x[i]));
-            // x[step].push_back(x[step - 1][i] + (1./24.)*k_full[0].x[i] + (5./48.)*k_full[3].x[i] + (27./56.)*k_full[4].x[i] + (125./336.)*k_full[5].x[i]);
+            x[step].push_back(x[step - 1][i] + ((1./24.))*k_full[0].x[i] + (5./48.)*k_full[3].x[i] + (27./56.)*k_full[4].x[i] + (125./336.)*k_full[5].x[i]);
             // x[step].push_back(x[step - 1][i] + (1. / 6.)*(k[0].x[i] + 4*k[2].x[i] + k[3].x[i]));
-            x[step].push_back(buf2.x[i]);
         }
 
         // cout << "priv pos " << pos << endl;
@@ -199,9 +184,9 @@ void RungeKutta(vector<vector<double> > &x, double C1, double C2, double alph, v
         // cout << "pos " << pos << endl;
     } while (fin == 0);
     // printf("pos = %.10f\n", pos);//10.677
-    // printf("pi = %.20lf\n", gig);//10.677
+    // printf("pi = %.20lf\n", M_PI);//10.677
     // cout << "with changes" << endl;
-    // cout << setprecision(20) << gig << "pa pam" << endl;;
+    // cout << setprecision(20) << M_PI << "pa pam" << endl;;
     // cout << "out" << endl;
 }
 
@@ -281,3 +266,80 @@ void invJac(double J[2][2]) {
     J[1][0] *= -1;
     J[1][1] = buf;
 }
+
+void RungeKutta2(vector<MyVector> &X, double C1, double C2, double alph, vector<double> &H) {
+    ofstream out("h.txt");
+    MyVector k_full[6], k_half_1[6], k_half_2[6],  buf(0, C1, 0, C2), E, k[6], full, half, buf2;
+    // vector<MyVector> X;
+    double pos = 0;
+	double h = 1. / 1000.;
+    int step = 0;
+    bool sw = 0, fin = 0;
+
+    X.push_back(buf);
+
+    do {
+        step ++;
+        sw = 0;
+
+        do {
+
+            if (pos + h > M_PI_2) {
+                h = M_PI_2 - pos;
+                fin = 1;
+            }
+
+            buf = X[step - 1];
+          
+            full = countStep(h, buf, alph);
+
+            half = countStep(h/2., buf, alph);
+            half = countStep(h/2., half, alph);
+
+
+            if (fin == 0) { 
+                if ((full + (-1)*half).norm() < EPS / K) {
+                // if (E.norm() < EPS / K) {
+                    cout << (full + (-1)*half).norm() << endl;
+                    h *= 2;
+                } else if ((full + (-1)*half).norm() > EPS) {
+                // } else if (E.norm() > EPS) {
+                    h /= 2;
+                } else {
+                    sw = 1;
+                }
+            } else {
+                sw = 1;
+            }
+
+            // out << "h: " << h << " " << E.x[0] << " "<< E.x[1] << " "<< E.x[2] << " "<< E.x[3] << " pos " << pos << endl;
+            out << "h: " << h << " pos: " << pos <<  " " << full.x[1] << " " << full.x[3] << endl;
+           
+        } while (sw == 0);
+
+
+        H.push_back(h);
+
+        X.push_back(full);
+
+        pos += h;
+
+    } while (fin == 0);
+
+    out.close();
+}
+
+MyVector countStep(double h, MyVector y, double alph) {
+    MyVector k[6];
+
+    k[0] = h*f(y, alph);
+    k[1] = h*f(y + (1./2.)*(k[0]), alph);
+    k[2] = h*f(y + (1./4.)*(k[0] + k[1]), alph);
+    k[3] = h*f(y + (-1)*k[1] + 2*k[2], alph);
+    k[4] = h*f(y + (1./27.)*(7*k[0] + 10*k[1] + k[3]), alph);
+    k[5] = h*f(y + (1./625.)*(28*k[0] + (-125)*k[1] + 546*k[2] + 54*k[3] + (-378)*k[4]), alph);
+
+    return y + (1./24.)*k[0] + (5./48.)*k[3] + (27./56.)*k[4] + (125./336.)*k[5];
+}
+
+double L(MyVector x_curr, MyVector x_next, double alph) {
